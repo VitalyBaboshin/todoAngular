21c9e8caa76a24da4b59ac9c3f9e688bed698011
VitaliyBaboshin <vitaliy2@ro.ru>
2020-06-28 21:46:09 +0500

    Add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent, HeroSearchComponent } from './components';
+import {HeroService, MessagesService, InMemoryDataService} from './services';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+    HeroSearchComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule,
+        HttpClientInMemoryWebApiModule.forRoot(
+          InMemoryDataService, {dataEncapsulation: false}
+        )
+    ],
+  providers: [
+    MessagesService,
+    HeroService,
+    InMemoryDataService
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
